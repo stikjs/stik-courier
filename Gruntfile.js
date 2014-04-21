41e59bbb0d55b197cc61ae92526f9b38f2019ea8
Lukas Alexandre <lukas@codelogic.me>
2014-04-21 15:31:10 +0200

    Extracting from stik-core

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt){
+  var srcFiles = [ 'src/courier.js' ];
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '<%= pkg.banner.divider %>' +
+              '<%= pkg.banner.project %>' +
+              '<%= pkg.banner.copyright %>' +
+              '<%= pkg.banner.license %>' +
+              '<%= pkg.banner.licenseLink %>' +
+              '<%= pkg.banner.divider %>' +
+              '\n' +
+              '// Version: <%= pkg.version %> | From: <%= grunt.template.today("dd-mm-yyyy") %>\n\n'
+    },
+    jasmine: {
+      src: [
+        'node_modules/stik-core.js/stik-core.js',
+        'node_modules/stik-labs.js/stik-labs.js'
+      ].concat(srcFiles),
+      options: {
+        specs: 'specs/*_spec.js',
+      }
+    },
+    concat: {
+      options: {
+        separator: '\n',
+        banner: '<%= meta.banner %>'
+      },
+      src: {
+        src: srcFiles,
+        dest: '<%= pkg.name %>'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= meta.banner %>',
+        mangle: false
+      },
+      stik: {
+        files: {
+         'stik-courier.min.js': ['stik-courier.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('test', 'jasmine');
+  grunt.registerTask('pack', ['concat', 'uglify']);
+};
